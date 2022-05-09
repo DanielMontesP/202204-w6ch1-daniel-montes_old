ddc10d288f733bc95f4b357ac0331bbf26f574d3
Daniel Montes <daniel.montes@gmail.com>
2022-05-10 00:12:04 +0200

    Add functionality to Todo component

--- src/componentes/Todos/Todos.js
+++ src/componentes/Todos/Todos.js
@@ -1,9 +1,19 @@
+import { useSelector } from "react-redux";
 import Todo from "../Todo/Todo";
 
 const Todos = () => {
-  // const todos = useSelector ((state) => state.projects);
-
-  return <Todo />;
+  const todosList = useSelector((state) => state.todos);
+  return (
+    <>
+      {todosList.map((todoCard) => {
+        return (
+          <li key={todoCard.id}>
+            <Todo id={todoCard.id} name={todoCard.name} done={todoCard.done} />
+          </li>
+        );
+      })}
+    </>
+  );
 };
 
 export default Todos;
